02c48c5bf2fa8629899caad29630ce62ee52f1d5
Prototest34 <Prototest34@gmail.com>
2022-05-15 21:11:45 +0200

    Optimise createReward methode

--- src/main/java/fr/norrion/daily_quests/model/quest/reward/QuestReward.java
+++ src/main/java/fr/norrion/daily_quests/model/quest/reward/QuestReward.java
@@ -4,6 +4,7 @@ import fr.norrion.daily_quests.model.quest.Quest;
 import org.bukkit.configuration.MemorySection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class QuestReward {
@@ -20,20 +21,16 @@ public abstract class QuestReward {
     public abstract void execute(Quest quest);
 
     static public List<QuestReward> create(MemorySection memorySection, String key) {
-        List<String> list = new ArrayList<>();
         List<QuestReward> res = new ArrayList<>();
-        for (String reward : memorySection.getString("reward_type").split(",")) {
-            if (list.stream().noneMatch(s -> s.equals(reward))) {
-                list.add(reward);
-            }
-        }
-        for (String reward : list) {
-            switch (QuestRewardType.valueOf(reward.toUpperCase())) {
-                case COMMAND -> res.add(QuestRewardCommand.createOne(memorySection, key));
-                case ITEMS -> res.add(QuestRewardItems.createOne(memorySection, key));
-                case MONEY -> res.add(QuestRewardMoney.createOne(memorySection, key));
-            }
-            ;
+        String rewardString = memorySection.getString("reward_type");
+        if (rewardString != null) {
+            Arrays.stream(rewardString.split(",")).distinct().forEach(s -> {
+                switch (QuestRewardType.valueOf(s.toUpperCase())) {
+                    case COMMAND -> res.add(QuestRewardCommand.createOne(memorySection, key));
+                    case ITEMS -> res.add(QuestRewardItems.createOne(memorySection, key));
+                    case MONEY -> res.add(QuestRewardMoney.createOne(memorySection, key));
+                }
+            });
         }
         return res;
     }
